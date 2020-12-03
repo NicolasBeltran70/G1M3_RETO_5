992b2491144884e5c7bd3530f74391acbd9fc00a
cindygovania <75170112+cindygovania@users.noreply.github.com>
2020-12-02 21:37:05 -0500

    Update RedflixApplication.java

--- reto5/src/main/java/com/g1m3/redflix/RedflixApplication.java
+++ reto5/src/main/java/com/g1m3/redflix/RedflixApplication.java
@@ -1,5 +1,6 @@
 package com.g1m3.redflix;
 
+import com.g1m3.redflix.vistas.Ventana1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @ComponentScan("com.g1m3.redflix")
 public class RedflixApplication {
-
-	public static void main(String[] args) {
+        
+        public static void main(String[] args) {
+		Ventana1 inter = new Ventana1();
+                inter.setVisible(true);
+	}
+        
+        public static void ServerSpring(String[] args) {
 		SpringApplication.run(RedflixApplication.class, args);
 	}
-
 }
